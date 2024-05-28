ff2ed49a6a13003f0b0585de0d19a4a82b8d1ec6
EuronRomanov <juanpablo.gaona@yahoo.com>
2024-05-28 15:12:36 -0500

    cambio en el archivo contact

--- app/screens/ContactsScreen.js
+++ app/screens/ContactsScreen.js
@@ -2,7 +2,10 @@ import {View, Text, StyleSheet, Button} from 'react-native'
 
 export const Contacts = ({navigation}) => {
     return <View style={styles.container}>
-        <Text>Estoy en contact</Text>
+        <Text >Bienvenido a mi aplicacion, soy JeanPierre</Text>
+        <Text style={styles.contacto1}>Pedro Sanchez</Text>
+        <Text style={styles.contacto2}>Felix Valdivieso</Text>
+        <Text style={styles.contacto3}>Jose Serrano</Text>
         <Button
             title='HOME'
             onPress={()=>{
@@ -16,9 +19,21 @@ export const Contacts = ({navigation}) => {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: 'red',
       alignItems: 'center',
       justifyContent: 'center',
     },
+    contacto1: {
+       
+        color: '#141823'
+      },
+      contacto2: {
+       
+        color: 'yellow'
+      },
+      contacto3: {
+       
+        color: 'black'
+      }
   });
   
