b54c5eceecfb3cd9f914ed22e96beb3d377b71b7
jjyanangomez <juanyasa_04@hotmail.com>
2024-05-28 15:08:04 -0500

    agregacion de productos y cambio de color de cada uno de ellos

--- app/screens/Product.js
+++ app/screens/Product.js
@@ -2,7 +2,10 @@ import {View, Text, StyleSheet, Button} from 'react-native'
 
 export const Product = ({navigation}) =>{
     return <View style={styles.container}> 
-        <Text>Estas en productos</Text>
+        <Text>Bienvenidos a mi tienda, soy Juan Yanangomez</Text>
+        <Text style={styles.colorProducto1}>Doritos</Text>
+        <Text style={styles.colorProducto2}>Chifles</Text>
+        <Text style={styles.colorProducto3}>Cristal beer</Text>
         <Button
             title='HOME'
             onPress={()=>{
@@ -20,4 +23,19 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+    colorProducto1:{
+        color: "brown",
+        marginBottom:10,
+        backgroundColor: 'chartreuse',
+    },
+    colorProducto2:{
+        color: "chocolate",
+        marginBottom:10,
+        backgroundColor: 'chartreuse',
+    },
+    colorProducto3:{
+        color: "darkblue",
+        marginBottom:10,
+        backgroundColor: 'chartreuse',
+    }
   });
