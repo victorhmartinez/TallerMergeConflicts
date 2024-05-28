84f2a1fe9d54c273629b56fc0e340759022310d5
Victor Martinez <victormartinezg1998@gmail.com>
2024-05-28 15:17:53 -0500

    Cambio de nombre Home

--- app/screens/Product.js
+++ app/screens/Product.js
@@ -9,7 +9,7 @@ export const Product = ({navigation}) =>{
         <Button
             title='HOME'
             onPress={()=>{
-                navigation.navigate('HomeNav');
+                navigation.navigate('WellcomeNav');
             }}
         />
     </View>
