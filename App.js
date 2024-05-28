f76479675b58752160d4fb5682adb707f5e12212
jjyanangomez <juanyasa_04@hotmail.com>
2024-05-28 15:35:40 -0500

    cambios en la navegacion del products

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name='WelcomeJeanPierre' component={Home} />
         <Stack.Screen name='CallNumber' component={Contacts} />
-        <Stack.Screen name='ProductNav' component={Product}/>
+        <Stack.Screen name='ProductNavigation' component={Product}/>
 
 
       </Stack.Navigator>
